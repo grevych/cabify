251d24c2cd6ac49a3c216a53080ac8f46aac5981
Gerardo Reyes <gerardo@100ladrillos.com>
2019-01-09 20:10:51 -0600

    Add storage factory

--- internal/storage/factory.go
+++ internal/storage/factory.go
@@ -6,19 +6,26 @@ import (
 	"github.com/grevych/cabify/internal/storage/memory"
 )
 
-var storages = map[string]func() Storage{
+var storages = map[string]func() (interface{}, error){
 	"memory": createMemory,
 }
 
 func Create(storageType string) (Storage, error) {
 	for _, storageFunc := range storages {
-		return storageFunc(), nil
+		customStorage, err := storageFunc()
+
+		storage, ok := customStorage.(Storage)
+		if !ok {
+			return nil, fmt.Errorf("Storage type %s is not a valid one", storageType)
+		}
+
+		return storage, err
 	}
 
 	return nil, fmt.Errorf("Storage type %s not found", storageType)
 }
 
-func createMemory() Storage {
+func createMemory() (interface{}, error) {
 	basketStore := memory.NewBasketStore()
 	productStore := memory.NewProductStore()
 
